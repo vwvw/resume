52386da4e9486de7cfd425014b543fccf74d2547
Nicolas Badoux <n.badoux@hotmail.com>
2018-11-12 23:20:55 +0100

    Mirgrating from semaphore to travis

--- semaphoreci_setup.sh
+++
@@ -1,3 +0,0 @@
-docker build -t texlive docker
-curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
-chmod +x dropbox_uploader.sh
